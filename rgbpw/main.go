f53210780cf9aa48d7d97b802775329594899406
Glyn Normington <gnormington@pivotal.io>
2016-10-26 16:56:35 +0100

    Initial commit
    Thanks to https://github.com/pivotalservices/cfops

---
+++ rgbpw/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"os"
+	"code.cloudfoundry.org/lager"
+
+	"github.com/pivotalservices/cfops/system"
+	"fmt"
+	"strings"
+)
+
+
+func main() {
+	logger := lager.NewLogger("Logs")
+	logger.RegisterSink(lager.NewWriterSink(os.Stderr, lager.ERROR))
+
+	if len(os.Args) < 2 || len(os.Args) > 4 || os.Args[1] == "help" {
+		fmt.Printf("Prints the UAA admin credentials, or just the password if the user name is 'admin', of a specified PCF instance.\n\nUsage: \n  %s <color | hostname> <ops manager userid> <ops manager password>\n", os.Args[0])
+		os.Exit(-1)
+	}
+
+	hostName := os.Args[1]
+	if !strings.Contains(hostName, ".") {
+		hostName = fmt.Sprintf("pcf.%s.springapps.io", hostName)
+	}
+
+	opsMgrUser := "pivotalcf"
+	if len(os.Args) > 2 {
+		opsMgrUser = os.Args[2]
+	}
+
+	opsMgrPassword := "pivotalcf"
+	if len(os.Args) > 2 {
+		opsMgrPassword = os.Args[2]
+	}
+
+	opsManager, err := system.NewOpsManagerClient(hostName, opsMgrUser, opsMgrPassword, logger)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create ops manager client: %s\n", err)
+		os.Exit(-2)
+	}
+
+	adminUser, adminPassword, err := opsManager.GetAdminCredentials()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to obtain admin credentials: %s\n", err)
+		os.Exit(-3)
+	}
+
+	if adminUser != "admin" {
+		fmt.Println("admin user: ", adminUser)
+	}
+
+	fmt.Println(adminPassword)
+	os.Exit(0)
+}
---
+++ system/opsmanager_client.go
@@ -0,0 +1,189 @@
+package system
+
+import (
+	"crypto/tls"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"code.cloudfoundry.org/lager"
+)
+
+type opsmanClient struct {
+	httpClient    httpClient
+	token         string
+	opsManagerURL string
+	logger        lager.Logger
+}
+
+type stagedProduct struct {
+	Type string `json:"type"`
+	GUID string `json:"GUID"`
+}
+
+type credentials struct {
+	Credential struct {
+		Type  string `json:"simple_credentials"`
+		Value struct {
+			Username string `json:"identity"`
+			Password string `json:"password"`
+		}
+	}
+}
+
+//NewOpsManagerClient ...
+func NewOpsManagerClient(hostname, username, password string, logger lager.Logger) (*opsmanClient, error) {
+	url := "https://" + hostname
+
+	token, err := getAuthorization(url, username, password, logger)
+
+	if err != nil {
+		return &opsmanClient{}, err
+	}
+
+	opsManClient := newHttpClient(url, token, logger)
+	return &opsmanClient{
+		token:         token,
+		httpClient:    opsManClient,
+		opsManagerURL: url,
+		logger:        logger,
+	}, nil
+}
+
+func (client *opsmanClient) GetStagedProducts() ([]stagedProduct, error) {
+	client.httpClient.NewRequest("api/v0/staged/products", nil)
+	stagedProducts := []stagedProduct{}
+	err := client.httpClient.Get(&stagedProducts)
+	return stagedProducts, err
+}
+
+type installationSettings struct {
+	Products products `json:"products"`
+}
+type products []product
+
+func (products products) CF() product {
+	for _, p := range products {
+		if strings.HasPrefix(p.InstallationName, "cf-") {
+			return p
+		}
+	}
+	panic("Can't find cf product in installation settings")
+	return product{}
+}
+
+type product struct {
+	InstallationName string `json:"installation_name"`
+	Jobs             jobs   `json:"jobs"`
+}
+type jobs []job
+
+func (jobs jobs) UAA() job {
+	for _, j := range jobs {
+		if j.InstallationName == "uaa" {
+			return j
+		}
+	}
+	panic("Can't find uaa job in product settings")
+	return job{}
+}
+
+type job struct {
+	InstallationName string `json:"installation_name"`
+	Properties       properties
+}
+
+type properties []property
+
+func (properties properties) AdminCredentials() property {
+	for _, p := range properties {
+		if p.Identifier == "admin_credentials" {
+			return p
+		}
+	}
+	panic("Can't find admin_credentials in uaa job")
+	return property{}
+}
+
+type property struct {
+	Identifier string          `json:"identifier"`
+	Value      json.RawMessage `json:"value"`
+}
+
+type credentialsValue struct {
+	Username string `json:"identity"`
+	Password string `json:"password"`
+}
+
+func (property property) Credentials() (username, password string, err error) {
+	creds := credentialsValue{}
+	//fmt.Printf("property.Value %s\n", string(property.Value))
+	err = json.Unmarshal(property.Value, &creds)
+	return creds.Username, creds.Password, err
+}
+
+func (client *opsmanClient) GetInstallationSettings() (installationSettings, error) {
+	client.httpClient.NewRequest("api/installation_settings", nil)
+	installationSetting := installationSettings{}
+	err := client.httpClient.Get(&installationSetting)
+	return installationSetting, err
+}
+
+func (client *opsmanClient) GetAdminCredentials() (username, password string, err error) {
+	installationSetting, err := client.GetInstallationSettings()
+	if err != nil {
+		return "", "", err
+	}
+	//fmt.Printf("installationSetting.Products.CF().Jobs %d\n", len(installationSetting.Products.CF().Jobs))
+	return installationSetting.Products.CF().Jobs.UAA().Properties.AdminCredentials().Credentials()
+}
+
+func getAuthorization(opsManagerURL, username, password string, logger lager.Logger) (string, error) {
+	body := url.Values{
+		"grant_type": {"password"},
+		"username":   {username},
+		"password":   {password},
+	}
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	opsManClient := &http.Client{Transport: tr}
+	request, err := http.NewRequest("POST", fmt.Sprintf("%s/uaa/oauth/token", opsManagerURL), strings.NewReader(body.Encode()))
+	if err != nil {
+		logger.Error("NewRequest failed:", err)
+		return "", err
+	}
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+	request.SetBasicAuth("opsman", "")
+	response, err := opsManClient.Do(request)
+	if err != nil {
+		logger.Error("POST failed:", err)
+		return "", err
+	}
+
+	if response.StatusCode == 200 {
+		responseToken := token{}
+		err = json.NewDecoder(response.Body).Decode(&responseToken)
+		if err != nil {
+			logger.Error("NewDecoder failed:", err)
+			return "", err
+		}
+		if responseToken.AccessToken == "" {
+			return "", fmt.Errorf("No token returned")
+		}
+		return "Bearer " + responseToken.AccessToken, nil
+	} else if response.StatusCode == 404 {
+		// Assume basic auth
+		auth := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
+		return "Basic " + auth, nil
+	} else {
+		return "", fmt.Errorf("Unexpected response code %d", response.StatusCode)
+	}
+}
+
+type token struct {
+	AccessToken string `json:"access_token,required"`
+}
